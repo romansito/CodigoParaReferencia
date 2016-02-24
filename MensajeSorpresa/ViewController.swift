855ae8fc4d3736b5e570758a3569541eb49e58a7
Roman Salazar Lopez <romansito18@gmail.com>
2016-02-24 15:14:08 -0800

    Codigo completo.

--- MensajeSorpresa/ViewController.swift
+++ MensajeSorpresa/ViewController.swift
@@ -9,16 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let frase: String = "Hola Mundo!"
 
+    @IBOutlet weak var etiqueta: UILabel!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func boton(sender: AnyObject) {
+        
+        etiqueta.text = frase
+        
+        
     }
+    
 
 
 }
